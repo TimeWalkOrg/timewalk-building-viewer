24d1aa204c99be6d23b0aa289031851bdf5b05e5
tedbarnett <mail@tedbarnett.com>
2019-06-02 14:10:56 -0700

    Working on Oculus Rift on Unity 2019.1.4f1.  Both Semester 01 and Semester 03 included.

---
+++ Assets/Scripts/timeWalkController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timeWalkController : MonoBehaviour
+{
+    //define a list where the prefabs will be stored
+    public static List<GameObject> myListObjects = new List<GameObject>();
+    public static int numSpawned = 0;
+    public static int numToSpawn = 3;
+    public static int currentObjectIndex = 0;
+    public static int objectsListLength = 0;
+    public static GameObject currentObject;
+
+    public AudioSource audioData;
+
+    void Start()
+    {
+        // NOTE: make sure all building prefabs are inside this folder: "Assets/Resources/Prefabs"
+
+        Object[] subListObjects = Resources.LoadAll("Prefabs", typeof(GameObject));
+        foreach (GameObject subListObject in subListObjects)
+        {
+            GameObject lo = (GameObject)subListObject;
+            myListObjects.Add(lo);
+            ++objectsListLength;
+        }
+        GameObject myObj = Instantiate(myListObjects[currentObjectIndex]) as GameObject;
+        currentObject = myObj;
+
+        audioData = GetComponent<AudioSource>();
+        audioData.Play(0);
+        // Debug.Log("started");
+    }
+
+    void SpawnNextObject()
+    {
+        Destroy(currentObject);
+        currentObjectIndex++;
+        if (currentObjectIndex == objectsListLength)
+        {
+            currentObjectIndex = 0;
+        }
+        GameObject myObj = Instantiate(myListObjects[currentObjectIndex]) as GameObject;
+        // myObj.transform.position = transform.position; // NO: instead we will use the object's default position
+        currentObject = myObj;
+        // Debug.Log("currentObjectIndex = " + currentObjectIndex);
+    }
+
+    void Update()
+    {
+        if (OVRInput.GetUp(OVRInput.Button.One, OVRInput.Controller.RTouch))
+        {
+            SpawnNextObject();
+        }
+    }
+
+}
---
+++ Assets/Scripts/timeWalkShowObjects.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timeWalkShowObjects : MonoBehaviour
+{
+    //define a list where the prefabs will be stored
+    public static List<GameObject> myListObjects = new List<GameObject>();
+
+    //I used this to keep track of the number of objects I spawned in the scene.
+    public static int numSpawned = 0;
+    public static int numToSpawn = 3;
+    public static int currentObjectIndex = 0;
+    public static int objectsListLength = 0;
+    public static GameObject currentObject;
+
+    public AudioSource audioData;
+
+    void Start()
+    {
+        // NOTE: make sure all building prefabs are inside this folder: "Assets/Resources/Prefabs"
+
+        Object[] subListObjects = Resources.LoadAll("Prefabs", typeof(GameObject));
+        foreach (GameObject subListObject in subListObjects)
+        {
+            GameObject lo = (GameObject)subListObject;
+            myListObjects.Add(lo);
+            ++objectsListLength;
+        }
+        GameObject myObj = Instantiate(myListObjects[currentObjectIndex]) as GameObject;
+        currentObject = myObj;
+
+        audioData = GetComponent<AudioSource>();
+        audioData.Play(0);
+        Debug.Log("started");
+    }
+
+
+
+    void SpawnNextObject()
+    {
+        Destroy(currentObject);
+        currentObjectIndex++;
+        if(currentObjectIndex == objectsListLength)
+        {
+            currentObjectIndex = 0;
+        }
+        GameObject myObj = Instantiate(myListObjects[currentObjectIndex]) as GameObject;
+        //myObj.transform.position = transform.position;
+        currentObject = myObj;
+    }
+
+    void Update()
+    {
+        if (OVRInput.GetUp(OVRInput.Button.One, OVRInput.Controller.RTouch))
+
+            // Add in test for keyboard press too ("N" for next building?)
+
+        {
+            // spawn the building prefab at origin position
+            // transform.position = new Vector3(0, 0, 0);
+            SpawnNextObject();
+        }
+    }
+
+}
